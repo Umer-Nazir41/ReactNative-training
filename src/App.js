dc5f825928e5c9f1d9e870c6b72c285b5c34e31c
umer-nazir41 <umer.nazir@systemsltd.com>
2021-10-29 16:23:19 +0500

    Redux added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Navigation from './navigation/Navigation';
 import {LogBox} from 'react-native';
+import {store} from './store/store';
 import {Provider} from 'react-redux';
 
 LogBox.ignoreAllLogs();
 
 class App extends React.Component {
   render() {
-    return <Navigation />;
+    return (
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
+    );
   }
 }
 
--- src/screen/Profile/Profile.js
+++ src/screen/Profile/Profile.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import styles from '../../styles/Index';
+import {Button, StyleSheet, Text, View} from 'react-native';
+import {increment, decrement} from '../../store/reducers/counterSlice';
+import {connect} from 'react-redux';
 
 //Profile Screen
 class Profile extends React.Component {
   render() {
     return (
-      <View style={styles.commonStyles.container}>
-        <Text>Hello From Profile</Text>
+      <View style={styles.container}>
+        <View style={[{width: '90%', margin: 10, backgroundColor: 'red'}]}>
+          <Button title="+" onPress={() => this.props.dispatch(increment())} />
+        </View>
+
+        <Text>{this.props.count}</Text>
+        <View style={[{width: '90%', margin: 10, backgroundColor: 'red'}]}>
+          <Button title="-" onPress={() => this.props.dispatch(decrement())} />
+        </View>
       </View>
     );
   }
 }
 
-export default Profile;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// export default Profile;
+const mapStateToProps = state => ({
+  count: state.counter.value,
+});
+
+export default connect(mapStateToProps)(Profile);
